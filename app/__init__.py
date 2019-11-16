9fd38816a0b7743643290a1274617e7d671c6a09
Daniel Adiletta <dan.adiletta@gmail.com>
2019-11-16 12:11:06 -0500

    new stack organized

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import datetime
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -56,6 +57,43 @@ def create_app(config_name):
     app.register_error_handler(500, crash_page)
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(403, page_forbidden)
+
+
+    # Executes before the first request is processed.
+    @app.before_first_request
+    def before_first_request():
+
+        # Create any database tables that don't exist yet.
+        db.create_all()
+
+        # Create the Roles "admin" and "end-user" -- unless they already exist
+        user_datastore.find_or_create_role(name='admin', description='Administrator')
+        user_datastore.find_or_create_role(name='end-user', description='End user')
+
+        # Create two Users for testing purposes -- unless they already exists.
+        # In each case, use Flask-Security utility function to encrypt the password.
+        encrypted_password = utils.encrypt_password(app.config['STARTING_ADMIN_PASS'])
+        if not user_datastore.get_user(app.config['STARTING_ADMIN1']):
+            user_datastore.create_user(email=app.config['STARTING_ADMIN1'], password=encrypted_password)
+        if not user_datastore.get_user(app.config['STARTING_ADMIN2']):
+            user_datastore.create_user(email=app.config['STARTING_ADMIN2'], password=encrypted_password)
+            
+
+        # Commit any database changes; the User and Roles must exist before we can add a Role to the User
+        db.session.commit()
+
+        # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
+        # Users already have these Roles.) Again, commit any database changes.
+        user_datastore.add_role_to_user(app.config['STARTING_ADMIN1'], 'admin')
+        confirmed_admin = user_datastore.get_user(app.config['STARTING_ADMIN1'])
+        confirmed_admin.confirmed_at = datetime.utcnow()
+
+        user_datastore.add_role_to_user(app.config['STARTING_ADMIN2'], 'admin')
+        confirmed_admin = user_datastore.get_user(app.config['STARTING_ADMIN2'])
+        confirmed_admin.confirmed_at = datetime.utcnow()
+
+        db.session.commit()
+
     
     return app
 
--- flaskinni.py
+++ flaskinni.py
@@ -11,7 +11,7 @@ import sys
 import click
 from datetime import datetime
 from flask_migrate import Migrate, upgrade
-from app import create_app, db, user_datastore
+from app import create_app, db
 from app.models import User, Role, Post, Tag
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'settings')
@@ -20,56 +20,8 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
+    # TODO: proper imports
     return dict(db=db, User=User, Role=Role)
-
-################
-# CUSTOM FILTERS
-# Example:
-# https://stackoverflow.com/questions/19394844/how-to-get-the-user-object-from-the-user-key-in-flask-template
-def get_user_by_user_key(user_key):
-    # your logic code here, e.g.
-    user = user_datastore.find_user(id=user_key)
-    if user.first_name:
-        return user.first_name + " " + user.last_name
-    else:
-        return "Anonymous"
-
-app.jinja_env.filters['get_name_by_key'] = get_user_by_user_key
-
-# Executes before the first request is processed.
-@app.before_first_request
-def before_first_request():
-
-    # Create any database tables that don't exist yet.
-    db.create_all()
-
-    # Create the Roles "admin" and "end-user" -- unless they already exist
-    user_datastore.find_or_create_role(name='admin', description='Administrator')
-    user_datastore.find_or_create_role(name='end-user', description='End user')
-
-    # Create two Users for testing purposes -- unless they already exists.
-    # In each case, use Flask-Security utility function to encrypt the password.
-    encrypted_password = utils.encrypt_password(app.config['STARTING_ADMIN_PASS'])
-    if not user_datastore.get_user(app.config['STARTING_ADMIN1']):
-        user_datastore.create_user(email=app.config['STARTING_ADMIN1'], password=encrypted_password)
-    if not user_datastore.get_user(app.config['STARTING_ADMIN2']):
-        user_datastore.create_user(email=app.config['STARTING_ADMIN2'], password=encrypted_password)
-        
-
-    # Commit any database changes; the User and Roles must exist before we can add a Role to the User
-    db.session.commit()
-
-    # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
-    # Users already have these Roles.) Again, commit any database changes.
-    user_datastore.add_role_to_user(app.config['STARTING_ADMIN1'], 'admin')
-    confirmed_admin = user_datastore.get_user(app.config['STARTING_ADMIN1'])
-    confirmed_admin.confirmed_at = datetime.utcnow()
-
-    user_datastore.add_role_to_user(app.config['STARTING_ADMIN2'], 'admin')
-    confirmed_admin = user_datastore.get_user(app.config['STARTING_ADMIN2'])
-    confirmed_admin.confirmed_at = datetime.utcnow()
-
-    db.session.commit()
 
 '''
 # to avoid reload issues on CSS updates
